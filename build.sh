f19628af49b376e3f4940925eb5c94d9b148ab93
zhaopan <syszhaopan@gmail.com>
2020-12-11 09:58:22 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+#set -e
 
 if [ "${DOMAIN}" == "**None**" ]; then
     echo "Please set DOMAIN"
@@ -8,11 +8,21 @@ fi
 
 cd /app
 
+go env -w GO111MODULE=on \
+
+go env -w GOPROXY=https://goproxy.cn,direct\
+
 go get -u github.com/golang/dep/cmd/dep \
 
 go mod tidy \
 
 cp config/config.example.toml config/config.toml \
+
+sed -i 's/broqiang.com/Eemor9uzSHv0ZqubFtbkfZ8uR0nezRwG/' config/config.toml \
+
+sed -i 's/需要填入你自己的备案号/渝ICP备14008201号/' config/config.toml \
+
+mkdir -p /app/resources/blog-docs \
 
 go build blog.go \
 
